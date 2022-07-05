6719191dec4e99fb039f58459599be62390d5a62
VilosmZX <jravaell@gmail.com>
2022-07-05 14:28:00 +0700

    Add handle event

--- server/index.js
+++ server/index.js
@@ -3,6 +3,18 @@ const io = require('socket.io');
 module.exports = function (http) {
     const socket = new io.Server(http);
     socket.on('connection', (client) => {
-        console.log(client.id);
+        client.on('disconnect', (reason) => {
+            console.log(`${client.id} is disconnected.`);
+        })
+        client.on('incoming_chat', (name, msg) => {
+            client.broadcast.emit('incoming_chat', name, msg);
+        })
+        client.on('change_name', (oldName, newName) => {
+            socket.emit('change_name', oldName, newName);
+        })
+        client.on('new_user', (name) => {
+            socket.emit('new_user', name);
+        })
     });
+    
 }
